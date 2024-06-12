8bbee239543b0520ba780eb1a2db69416da7d8cf
mugdha2025 <153657567+mugdha2025@users.noreply.github.com>
2024-06-12 16:59:04 +0000

    added new function called appendNumber

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let currentOperator = null;
 let displayValue = '0';
 
 function add(a, b) {
-    
+
     return a + b;
 }
 
@@ -23,16 +23,16 @@ function multiply(a, b) {
     return a * b;
 }
 
-function updateDisplay(){
+function updateDisplay() {
     const display = document.getElementById("display");
     display.textContent = displayValue;
 }
 
-function appendNumber(number){
-    if (currentOperator === null){
+function appendNumber(number) {
+    if (currentOperator === null) {
         firstNumb += number;
         displayValue = firstNumb;
-    }else{
+    } else {
         scndNumb += number;
         displayValue = scndNumb;
     }
@@ -40,18 +40,18 @@ function appendNumber(number){
 
 }
 
-function appendOperator(operator){
-    if (currentOperator !== null && scndNumb !== ''){
+function appendOperator(operator) {
+    if (currentOperator !== null && scndNumb !== '') {
         ongotpointercapture();
     }
     currentOperator = operator;
-    if(firstNumb !== '' && scndNumb !== ''){
+    if (firstNumb !== '' && scndNumb !== '') {
         displayValue = firstNumb;
     }
     updateDisplay();
 }
 
-function clearDisplay(){
+function clearDisplay() {
     firstNumb = '';
     scndNumb = '';
     currentOperator = null;
@@ -59,21 +59,21 @@ function clearDisplay(){
     updateDisplay();
 }
 
-function operate(){
+function operate() {
     let numb1 = parseFloat(firstNumb);
-    let numb2 = scndNumb !== ''? parseFloat(scndNumb) : numb1;
-    
+    let numb2 = scndNumb !== '' ? parseFloat(scndNumb) : numb1;
+
     if (isNaN(numb1) || isNaN(numb2)) return;
 
     let result;
 
-    if (currentOperator === '+'){
+    if (currentOperator === '+') {
         result = add(numb1, numb2);
-    } else if(currentOperator === '-'){
+    } else if (currentOperator === '-') {
         result = subtract(numb1, numb2);
-    } else if(currentOperator === '*'){
+    } else if (currentOperator === '*') {
         result = multiply(numb1, numb2);
-    }else if(currentOperator === '/'){
+    } else if (currentOperator === '/') {
         result = divide(numb1, numb2);
     }
 
